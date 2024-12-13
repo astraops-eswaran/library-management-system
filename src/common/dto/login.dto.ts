a2bf7607938c6df8e4f27a49644f2ca640282823
user-ao-001 <eswrana@astraops.ai>
2024-12-13 15:37:54 +0530

    initial commit

--- src/common/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const LoginSchema = z.object({
-    username: z.string(),
-    password: z.string(),
-});
-
-export type LoginSchema = z.infer<typeof LoginSchema>;
-export type LoginDto = LoginSchema;
--- src/common/interfaces/jwt-payload.interface.ts
+++
@@ -1,7 +0,0 @@
-
-//this interface is used to define the payload of the jwt token
-export interface JwtPayload{
-    sub:string,
-    role:string,
-    username:string,
-}
